6cc3f44bd94cab3315552814fe0a112c4394df65
mauro.lavin <villarroelmauroemanuel@gmail.com>
2022-06-29 23:39:46 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//Desafio complementario algoritmo
+//conversor de pixeles a cm
+
+for(let i = 0;;){
+    let pixel = prompt("Cantidad de pixeles a convertir a centimetros?");
+    let calculo = (pixel * 0.00846);
+    let resultado = calculo;
+
+    alert("El resultado es " + resultado);
+}
